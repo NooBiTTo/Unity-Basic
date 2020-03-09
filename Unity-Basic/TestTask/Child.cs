9e24b5251eb5b2260777ecf8bf4fbf6b5fbccec4
Kanonik Alexandr <sashakanonik@mail.ru>
2020-03-09 23:51:17 +0300

    Child class created

---
+++ Unity-Basic/TestTask/Child.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask
+{
+    public class Child
+    {
+        public string _name;
+        public List<int> Bill { get; set; }
+        
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value == "")
+                    throw new Exception("Имя не может быть пустым");
+                else
+                    _name = value;
+            }
+        }
+
+        public Child(string name)
+
+        {
+            Name = name;
+            Bill = new List<int>();
+        }
+
+        public List<int> addApples (int numberOfApples)
+        {
+            if (Bill.Count == 0)
+                Bill.Add(numberOfApples);
+            else
+                Bill.Add(Bill[Bill.Count - 1] + numberOfApples);
+            return Bill;
+        }
+
+    }
+}
--- Unity-Basic/TestTask/Program.cs
+++ Unity-Basic/TestTask/Program.cs
@@ -10,7 +10,10 @@ namespace TestTask
     {
         static void Main(string[] args)
         {
-
+            Child masha = new Child("Masha");
+            masha.addApples(5);
+            masha.addApples(10);
+            masha.addApples(20);
         }
     }
 }
